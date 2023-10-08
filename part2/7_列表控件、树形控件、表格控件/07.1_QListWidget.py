a0ed07712d5a34d6e8006aee1649126fe11fcb69
wanqiu.xia <1071744582@qq.com>
2023-10-08 13:58:14 +0800

    列表控件

--- part2/7_列表控件、树形控件、表格控件/07.1_QListWidget.py
+++ part2/7_列表控件、树形控件、表格控件/07.1_QListWidget.py
@@ -1,36 +1,48 @@
-# -*- coding: utf-8 -*-
-"""
-列表控件可以让我们以列表形式呈现内容，使界面更加有序美观。QListWidget列表控件应当与QListWidgetItem一起使用，后者作为项被添加入列表控件中，
-也就是说列表控件中的每一项都是一个QListWidgetItem。这也是为什么我们说QListWidget是一个基于项(Item-based)的控件了。
-
-同样基于项的控件还有QTreeWidget树形控件和QTableWidget表格控件，前者以树状方式呈现内容，并与QTreeWidgetItem搭配使用；
-后者以表格形式呈现内容，并与QTableWidgetItem一起使用。
-"""
 import sys
 from PySide2.QtGui import QPixmap
 from PySide2.QtWidgets import QApplication, QWidget, QLabel, QListWidget, QListWidgetItem, QHBoxLayout
 
 
 class Demo(QWidget):
-    def __int__(self):
+    def __init__(self):
         super(Demo, self).__init__()
         self.pic_label = QLabel(self)                       # 1
         self.pic_label.setPixmap(QPixmap('arrow.png'))
 
         self.listwidget_1 = QListWidget(self)               # 2
         self.listwidget_2 = QListWidget(self)
+        self.listwidget_1.doubleClicked.connect(lambda: self.change_func(self.listwidget_1))
+        self.listwidget_2.doubleClicked.connect(lambda: self.change_func(self.listwidget_2))
 
+        for i in range(6):                                  # 3
+            text = 'Item {}'.format(i)
+            self.item = QListWidgetItem(text)
+            self.listwidget_1.addItem(self.item)
 
+        self.item_6 = QListWidgetItem('Item 6', self.listwidget_1)  # 4
 
+        self.listwidget_1.addItem('Item 7')                         # 5
+        str_list = ['Item 9', 'Item 10']
+        self.listwidget_1.addItems(str_list)
 
+        self.item_8 = QListWidgetItem('Item 8')                     # 6
+        self.listwidget_1.insertItem(8, self.item_8)
+        # self.listwidget_1.insertItem(8, 'Item 8')
 
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.listwidget_1)
+        self.h_layout.addWidget(self.pic_label)
+        self.h_layout.addWidget(self.listwidget_2)
+        self.setLayout(self.h_layout)
 
-
-
-
-
-
-
+    def change_func(self, listwidget):                              # 7
+        if listwidget == self.listwidget_1:
+            item = QListWidgetItem(self.listwidget_1.currentItem())
+            self.listwidget_2.addItem(item)
+            print(self.listwidget_2.count())
+        else:
+            self.listwidget_2.takeItem(self.listwidget_2.currentRow())
+            print(self.listwidget_2.count())
 
 
 if __name__ == '__main__':
@@ -38,3 +50,20 @@ if __name__ == '__main__':
     demo = Demo()
     demo.show()
     sys.exit(app.exec_())
+
+
+"""
+1. pic_label用于显示图片；
+
+2. 实例化两个QListWidget，listwidget_1放在左边用于显示可选的内容，listwidget_2放在右边用于显示被双击的项。
+    然后将这两个QListWidget控件的doubleClicked信号和自定义的槽函数连接起来，每当双击QListWidget中的某项时，就会触发该槽函数。
+
+3. 循环创建六个QListWidgetItem，并通过调用addItem(QListWidgetItem)将其添加到listwidget_1中；
+
+4. 当然也可以通过实例化时直接指定父类的方式进行添加；
+
+5. 也可以不用QListWidgetItem，直接调用addItem(str)方法来添加一项内容。
+    也可以使用addItem(Iterable)来添加一组项内容(不过若要让项呈现更多功能的话，还是应该选择QListWidgetItem)；
+
+6. 通过insertItem(row, QListWidgetItem)方法可以在指定行中加入一项内容；
+"""
